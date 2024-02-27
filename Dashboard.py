bdf802cc0f8be2ed0cab0fa35edce024c4cf606b
rounakpaul11 <142620722+rounakpaul11@users.noreply.github.com>
2024-02-27 13:36:25 +0530

    CHANGED OS.CHDIR

--- Dashboard.py
+++ Dashboard.py
@@ -16,7 +16,7 @@ if fl is not None:
     st.write(filename)
     df = pd.read_csv(filename, encoding = "ISO-8859-1")
 else:
-    os.chdir(r"C:\Users\ranja\Desktop\streamlit dashboard\Streamlit-dashboard")
+   # os.chdir(r"C:\Users\ranja\Desktop\streamlit dashboard\Streamlit-dashboard")
     df = pd.read_excel("Superstore.xls")
 
 
