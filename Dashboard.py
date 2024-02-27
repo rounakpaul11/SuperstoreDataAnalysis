e7660d12155ec6d62bc0de3ae3a4b686b2afaf37
rounakpaul11 <142620722+rounakpaul11@users.noreply.github.com>
2024-02-27 14:58:05 +0530

    3.7.2

--- Dashboard.py
+++ Dashboard.py
@@ -115,11 +115,11 @@ with st.expander("View Data of TimeSeries:"):
     st.download_button('Download Data', data = csv, file_name = "TimeSeries.csv", mime ='text/csv')
 
 # Create a treem based on Region, category, sub-Category
-st.subheader("Hierarchical view of Sales using TreeMap")
-fig3 = px.treemap(filtered_df, path = ["Region","Category","Sub-Category"], values = "Sales",hover_data = ["Sales"],
-                  color = "Sub-Category")
-fig3.update_layout(width = 800, height = 650)
-st.plotly_chart(fig3, use_container_width=True)
+#st.subheader("Hierarchical view of Sales using TreeMap")
+#fig3 = px.treemap(filtered_df, path = ["Region","Category","Sub-Category"], values = "Sales",hover_data = ["Sales"],
+ #                 color = "Sub-Category")
+#fig3.update_layout(width = 800, height = 650)
+#st.plotly_chart(fig3, use_container_width=True)
 
 chart1, chart2 = st.columns((2))
 with chart1:
